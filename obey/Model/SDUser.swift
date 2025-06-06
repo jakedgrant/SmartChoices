dccea6883f73f621aff0b90c8d9c9f8015dda063
Jake Grant <8376905+jakedgrant@users.noreply.github.com>
2025-06-06 00:09:05 -0500

    Fix indentation and formatting

---
+++ obey/Model/SDUser.swift
@@ -0,0 +1,38 @@
+import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+final class SDUser: Identifiable {
+    var id = UUID()
+    var name: String = ""
+    var color: CodableColor = CodableColor(.blue)
+    var odds: Int = Constants.startingOdds
+    var losses: Int = 0
+
+    @Relationship(deleteRule: .nullify, inverse: \SDReward.users)
+    var rewards: [SDReward]? = []
+
+    @Relationship(deleteRule: .nullify, inverse: \SDLog.user)
+    var logs: [SDLog]? = []
+
+    init(
+        name: String = "",
+        color: CodableColor = CodableColor(.blue),
+        odds: Int = Constants.startingOdds,
+        losses: Int = 0,
+        rewards: [SDReward]? = [],
+        logs: [SDLog]? = []
+    ) {
+        self.name = name
+        self.color = color
+        self.odds = odds
+        self.losses = losses
+        self.rewards = rewards
+        self.logs = logs
+    }
+}
+
+extension SDUser {
+    var swiftUIColor: Color { color.color }
+}
--- obeyWatch Watch App/obeyWatchApp.swift
+++ obeyWatch Watch App/obeyWatchApp.swift
@@ -11,11 +11,11 @@ import SwiftUI
 @main
 struct obeyWatch_Watch_AppApp: App {
 
-	
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-		.modelContainer(for: [SDReward.self, SDLog.self])
-	}
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: [SDReward.self, SDLog.self, SDUser.self])
+    }
 }
