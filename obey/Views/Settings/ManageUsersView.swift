a2e054964f6212ef2354b74dc7c9b2dba298292b
jakedgrant <jakegrant@hey.com>
2025-06-15 17:11:17 -0500

    change filter on manage rewards view change nav title style in manage rewards and manage users

--- obey/Views/Settings/ManageUsersView.swift
+++ obey/Views/Settings/ManageUsersView.swift
@@ -33,6 +33,7 @@ struct ManageUsersView: View {
 			}
 		}
 		.navigationTitle(Text("Manage Users"))
+        .navigationBarTitleDisplayMode(.inline)
 		.navigationDestination(for: SDUser.self) { AddEditUserView(user: $0) }
 		.alert("Delete user", isPresented: Binding(
 			get: { userToDelete != nil },
