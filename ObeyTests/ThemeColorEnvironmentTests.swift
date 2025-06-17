9a92963c1a7c5ec26ea318913a999a02e51e4a5a
Jake Grant <8376905+jakedgrant@users.noreply.github.com>
2025-06-16 22:51:21 -0500

    Replace CodableColor with StandardColor (#26)

--- ObeyTests/ThemeColorEnvironmentTests.swift
+++ ObeyTests/ThemeColorEnvironmentTests.swift
@@ -8,17 +8,17 @@ struct ThemeColorEnvironmentTests {
     func themeColorReflectsSelectedUser() async throws {
         let manager = SelectedUserManager.shared
         defer { manager.selectedUser = nil }
-        let redUser = SDUser(name: "Red", color: CodableColor(.red))
-        let greenUser = SDUser(name: "Green", color: CodableColor(.green))
+        let redUser = SDUser(name: "Red", color: .red)
+        let greenUser = SDUser(name: "Green", color: .green)
 
         manager.selectedUser = redUser
         var env = EnvironmentValues()
         env.themeColor = manager.selectedUser?.color.color ?? .accentColor
-        #expect(env.themeColor == CodableColor(.red).color)
+        #expect(env.themeColor == StandardColor.red.color)
 
         manager.selectedUser = greenUser
         env.themeColor = manager.selectedUser?.color.color ?? .accentColor
-        #expect(env.themeColor == CodableColor(.green).color)
+        #expect(env.themeColor == StandardColor.green.color)
     }
 
     @MainActor
@@ -26,15 +26,15 @@ struct ThemeColorEnvironmentTests {
     func themeColorReflectsUserColorChange() async throws {
         let manager = SelectedUserManager.shared
         defer { manager.selectedUser = nil }
-        let user = SDUser(name: "Kid", color: CodableColor(.blue))
+        let user = SDUser(name: "Kid", color: .blue)
         manager.selectedUser = user
 
         var env = EnvironmentValues()
         env.themeColor = manager.selectedUser?.color.color ?? .accentColor
-        #expect(env.themeColor == CodableColor(.blue).color)
+        #expect(env.themeColor == StandardColor.blue.color)
 
-        user.color = CodableColor(.orange)
+        user.color = .orange
         env.themeColor = manager.selectedUser?.color.color ?? .accentColor
-        #expect(env.themeColor == CodableColor(.orange).color)
+        #expect(env.themeColor == StandardColor.orange.color)
     }
 }
---
+++ obey/Model/StandardColor.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+enum StandardColor: String, CaseIterable, Codable {
+    case red, orange, yellow, green, mint, teal, cyan, blue, indigo, purple, pink, brown, gray
+
+    var color: Color {
+        switch self {
+        case .red: return .red
+        case .orange: return .orange
+        case .yellow: return .yellow
+        case .green: return .green
+        case .mint: return .mint
+        case .teal: return .teal
+        case .cyan: return .cyan
+        case .blue: return .blue
+        case .indigo: return .indigo
+        case .purple: return .purple
+        case .pink: return .pink
+        case .brown: return .brown
+        case .gray: return .gray
+        }
+    }
+
+#if canImport(UIKit)
+    init(_ color: Color) {
+        let uiColor = UIColor(color)
+        if uiColor.isEqual(UIColor.red) { self = .red }
+        else if uiColor.isEqual(UIColor.orange) { self = .orange }
+        else if uiColor.isEqual(UIColor.yellow) { self = .yellow }
+        else if uiColor.isEqual(UIColor.green) { self = .green }
+//        else if uiColor.isEqual(UIColor.systemMint) { self = .mint }
+//        else if uiColor.isEqual(UIColor.systemTeal) { self = .teal }
+        else if uiColor.isEqual(UIColor.cyan) { self = .cyan }
+        else if uiColor.isEqual(UIColor.blue) { self = .blue }
+//        else if uiColor.isEqual(UIColor.systemIndigo) { self = .indigo }
+        else if uiColor.isEqual(UIColor.purple) { self = .purple }
+//        else if uiColor.isEqual(UIColor.systemPink) { self = .pink }
+        else if uiColor.isEqual(UIColor.brown) { self = .brown }
+        else if uiColor.isEqual(UIColor.gray) { self = .gray }
+        else { self = .mint }
+    }
+#endif
+}
+
