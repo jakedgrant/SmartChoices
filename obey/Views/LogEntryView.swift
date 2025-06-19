5b65b227748a8382056eb84d30b20e87ee8cf23f
Jake Grant <8376905+jakedgrant@users.noreply.github.com>
2025-06-19 12:43:53 -0500

    Show rewarded user in log history (#29)

--- obey/Views/LogEntryView.swift
+++ obey/Views/LogEntryView.swift
@@ -8,107 +8,113 @@
 import SwiftUI
 
 struct LogEntryView: View {
-	
-	let timestamp: Date?
-	let name: String?
-	let systemImage: String?
-	
-	let odds: Int?
-	let losses: Int?
-	let increasedOdds: Bool?
-	
-	var body: some View {
-		
-		HStack(alignment: .top, spacing: 12) {
-			
-			ImageView(systemName: systemImage)
-			
-			VStack(alignment: .leading, spacing: 4) {
-					
-				Text(name ?? "A reward")
-					.bold()
-					.font(.headline)
-				
-				if let timestamp {
-					Text(timestamp.formatted(date: .numeric, time: .shortened))
-						.font(.subheadline)
-						.foregroundStyle(.gray)
-				}
-				
-				if let odds, let losses {
-					OddsView(odds: odds, losses: losses, increasedOdds: increasedOdds)
-				}
-			}
-			
-			Spacer()
-		}
-		.fontDesign(.rounded)
-	}
-	
-	private struct ImageView: View {
-		let systemName: String?
-		
-		var body: some View {
-			Image(systemName: systemName ?? Constants.defaultImageName)
-				.foregroundStyle(Color.white)
-				.bold()
-				.fontDesign(.rounded)
-				.padding()
-				.background(SCButtonBackground())
-				.overlay(
-					Circle()
-						.stroke(.black.opacity(0.2), lineWidth: 8.0)
-				)
-				.clipShape(Circle())
-		}
-	}
-	
-	private struct OddsView: View {
-		let odds: Int
-		let losses: Int
-		let increasedOdds: Bool?
-		
-		var body: some View {
-			HStack(spacing: 4) {
-				BadgeLabel("1 : \(odds)", systemImage: "dice")
-				BadgeLabel("\(losses)", systemImage: "star.slash")
-				
-				if increasedOdds == true {
-					BadgeLabel(nil, systemImage: "arrow.up")
-				}
-			}
-			.font(.footnote)
-		}
-	}
+    
+    let timestamp: Date?
+    let name: String?
+    let systemImage: String?
+    
+    let odds: Int?
+    let losses: Int?
+    let increasedOdds: Bool?
+    let userName: String?
+    let userColor: Color?
+    
+    var body: some View {
+        
+        Label {
+            
+            VStack(alignment: .leading, spacing: 4) {
+                
+                Text(name ?? "A reward")
+                    .bold()
+                    .font(.headline)
+                
+                if let timestamp {
+                    Text(timestamp.formatted(date: .numeric, time: .shortened))
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
+                
+                if let odds, let losses {
+                    OddsView(userName: userName, userColor: userColor, odds: odds, losses: losses, increasedOdds: increasedOdds)
+                }
+            }
+            
+        } icon: {
+            Image(systemName: systemImage ?? Constants.defaultImageName)
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(userColor ?? .accentColor)
+        }
+        .fontDesign(.rounded)
+    }
+    
+    
+    private struct OddsView: View {
+        let userName: String?
+        let userColor: Color?
+        let odds: Int
+        let losses: Int
+        let increasedOdds: Bool?
+        
+        var body: some View {
+            HStack(spacing: 4) {
+                if let userName {
+                    HStack(spacing: 4) {
+                        Text(userName)
+                    }
+                    .foregroundStyle(userColor ?? .primary)
+                    .padding(.horizontal, 3)
+                    .padding(.vertical, 3)
+                    .background(
+                        RoundedRectangle(cornerRadius: 4.0, style: .continuous)
+                            .fill((userColor ?? .primary).opacity(0.2))
+                    )
+                }
+                BadgeLabel("1 : \(odds)", systemImage: "dice")
+                BadgeLabel("\(losses)", systemImage: "star.slash")
+                
+                if increasedOdds == true {
+                    BadgeLabel(nil, systemImage: "arrow.up")
+                }
+            }
+            .font(.footnote)
+        }
+    }
 }
 
 #Preview {
-	List {
-		LogEntryView(
-			timestamp: .now.addingTimeInterval(-60),
-			name: "Pillow fight",
-			systemImage: "figure.boxing",
-			odds: 5,
-			losses: 2,
-			increasedOdds: true
-		)
-		
-		LogEntryView(
-			timestamp: .now.addingTimeInterval(-60),
-			name: "Pillow fight",
-			systemImage: "figure.boxing",
-			odds: 5,
-			losses: 2,
-			increasedOdds: true
-		)
-		
-		LogEntryView(
-			timestamp: .now.addingTimeInterval(-60),
-			name: "Pillow fight",
-			systemImage: "figure.boxing",
-			odds: 5,
-			losses: 2,
-			increasedOdds: false
-		)
-	}
+    List {
+        LogEntryView(
+            timestamp: .now.addingTimeInterval(-60),
+            name: "Pillow fight",
+            systemImage: "figure.boxing",
+            odds: 5,
+            losses: 2,
+            increasedOdds: true,
+            userName: "Kid",
+            userColor: .red
+        )
+        
+        LogEntryView(
+            timestamp: .now.addingTimeInterval(-60),
+            name: "Pillow fight",
+            systemImage: "figure.boxing",
+            odds: 5,
+            losses: 2,
+            increasedOdds: true,
+            userName: "Kid",
+            userColor: .blue
+        )
+        
+        LogEntryView(
+            timestamp: .now.addingTimeInterval(-60),
+            name: "Pillow fight",
+            systemImage: "figure.boxing",
+            odds: 5,
+            losses: 2,
+            increasedOdds: false,
+            userName: "Kid",
+            userColor: .green
+        )
+    }
 }
