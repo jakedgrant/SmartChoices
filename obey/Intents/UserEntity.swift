dea37f3637da0cf41db943bfaf6ad23263d1a998
jakedgrant <jakegrant@hey.com>
2025-06-21 22:48:20 -0500

    fix indentation

--- obey/Intents/UserEntity.swift
+++ obey/Intents/UserEntity.swift
@@ -3,29 +3,29 @@ import Foundation
 import SwiftUI
 
 struct UserEntity: AppEntity, Displayable {
-
-        static var typeDisplayRepresentation: TypeDisplayRepresentation {
-                "User"
-        }
-
-        static var defaultQuery = UserQuery()
-        var id: SDUser.ID
-
-        @Property(title: "Name")
-        var name: String
-
-        var image: String { "person.crop.circle" }
-        var description: String { name }
-
-        var displayRepresentation: DisplayRepresentation {
-                DisplayRepresentation(
-                        title: "\(name)",
-                        image: DisplayRepresentation.Image(systemName: image)
-                )
-        }
-
-        init(from user: SDUser) {
-                self.id = user.id
-                self.name = user.name
-        }
+    
+    static var typeDisplayRepresentation: TypeDisplayRepresentation {
+        "User"
+    }
+    
+    static var defaultQuery = UserQuery()
+    var id: SDUser.ID
+    
+    @Property(title: "Name")
+    var name: String
+    
+    var image: String { "person.crop.circle" }
+    var description: String { name }
+    
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(
+            title: "\(name)",
+            image: DisplayRepresentation.Image(systemName: image)
+        )
+    }
+    
+    init(from user: SDUser) {
+        self.id = user.id
+        self.name = user.name
+    }
 }
--- obey/Intents/UserQuery.swift
+++ obey/Intents/UserQuery.swift
@@ -2,18 +2,18 @@ import AppIntents
 import Foundation
 
 struct UserQuery: EntityQuery {
-
-        func entities(for identifiers: [UserEntity.ID]) async throws -> [Entity] {
-                let db = try UserDatabase()
-                let users = db.users(with: identifiers)
-
-                return users.map { UserEntity(from: $0) }
-        }
-
-        func suggestedEntities() async throws -> [UserEntity] {
-                let db = try UserDatabase()
-                let users = db.allUsers()
-
-                return users.map { UserEntity(from: $0) }
-        }
+    
+    func entities(for identifiers: [UserEntity.ID]) async throws -> [Entity] {
+        let db = try UserDatabase()
+        let users = db.users(with: identifiers)
+        
+        return users.map { UserEntity(from: $0) }
+    }
+    
+    func suggestedEntities() async throws -> [UserEntity] {
+        let db = try UserDatabase()
+        let users = db.allUsers()
+        
+        return users.map { UserEntity(from: $0) }
+    }
 }
