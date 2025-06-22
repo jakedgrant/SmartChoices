627550a5d0fac73a397016917bca8a7b2c7aa7a8
Jake Grant <8376905+jakedgrant@users.noreply.github.com>
2025-06-22 14:13:03 -0500

    Filter rewards by user and identifiers

--- obey/Model/DB/RewardDatabase.swift
+++ obey/Model/DB/RewardDatabase.swift
@@ -68,20 +68,20 @@ extension RewardDatabase {
 //		}
 //	}
 //	
-//	func rewards(with identifiers: [T.ID]) -> [T] {
-//		let predicate = #Predicate<T> { t in
-//			identifiers.contains(t.id)
-//		}
-//		
-//		let sort = SortDescriptor<T>(\.name)
-//		
-//		do {
-//			return try self.read(predicate: predicate, sortDescriptors: sort)
-//		} catch {
-//			print(error.localizedDescription)
-//			return []
-//		}
-//	}
+	func rewards(with identifiers: [T.ID]) -> [T] {
+		let predicate = #Predicate<T> { t in
+			identifiers.contains(t.id)
+		}
+		
+		let sort = SortDescriptor<T>(\.name)
+		
+		do {
+			return try self.read(predicate: predicate, sortDescriptors: sort)
+		} catch {
+			print(error.localizedDescription)
+			return []
+		}
+	}
 }
 
 extension RewardDatabase {
