b104177e7b10f861353d1335c970da0662a2d9ce
jakedgrant <jakegrant@hey.com>
2025-06-26 00:17:35 -0500

    who knows

--- obeyWatch Watch App/ContentView.swift
+++ obeyWatch Watch App/ContentView.swift
@@ -9,115 +9,113 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-	@AppStorage("odds", store: UserDefaults(suiteName: Constants.suiteName)) var odds: Int = Constants.startingOdds
-	@AppStorage("losses", store: UserDefaults(suiteName: Constants.suiteName)) var losses: Int = 0
-	
-	@Environment(\.modelContext) var modelContext
-	@Query var rewards: [SDReward]
-	@Environment(\.themeColor) private var themeColor
-	
-	@State private var isShowingRewards = false
-	@State private var isAlerting = false
-
-        var body: some View {
-                NavigationStack {
-                        TabView {
-                                rollView
-                                        .tabItem { Label("Roll", systemImage: "die.face.5") }
-
-                                SwitchUserView()
-                                        .tabItem { Label("Users", systemImage: "person.crop.circle") }
-
-                                StatsView()
-                                        .tabItem { Label("Stats", systemImage: "chart.bar") }
-                        }
-                        .tabViewStyle(.verticalPage)
-			
-			.alert(
-				Alert.unlucky.title,
-				isPresented: $isAlerting,
-				presenting: Alert.unlucky) { state in
-					
-					Button(state.cta) {	}
-				} message: { state in
-					Text(state.subtitle)
-				}
-			
-				.sheet(isPresented: $isShowingRewards) {
-					RewardListView(modelContext: self.modelContext)
-				}
-			
-				.containerBackground(
-					isAlerting
-					? themeColor.gradient
-					: Color.gray.gradient,
-					for: .navigation
-				)
-			
-				.scenePadding()
-			
-                                .task {
-                                        await populateRewards()
-                                }
+    
+    @Environment(\.themeColor) var themeColor
+    @Environment(\.modelContext) var modelContext
+    @Query var rewards: [SDReward]
+    @ObservedObject private var selectedUserManager = SelectedUserManager.shared
+    @State private var isSwitchingUser = false
+    
+    var body: some View {
+        NavigationStack {
+            TabView {
+                
+                RollView(selectedUser: selectedUserManager.selectedUser)
+                    .containerBackground(themeColor.gradient, for: .tabView)
+                
+                List {
+                    StatsView()
                 }
-        }
-
-        private var rollView: some View {
-                VStack {
-                        Spacer()
-
-                        Button("Smart Choice", action: roll)
-                                .bold()
-                                .fontDesign(.rounded)
-                                .focusable()
-
-                        Spacer()
+                .navigationTitle("Stats for \(selectedUserManager.selectedUser?.name ?? "All")")
+                .tint(themeColor)
+            }
+            .tabViewStyle(.verticalPage)
+            .sheet(isPresented: $isSwitchingUser) {
+                SwitchUserView()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        isSwitchingUser = true
+                    } label: {
+                        Label("Switch User", systemImage: "person.crop.circle")
+                            .foregroundStyle(.primary)
+                    }
                 }
-                .containerBackground(themeColor.gradient, for: .tabView)
+            }
         }
-	
-	private func roll() {
-		let result = Int.random(in: 1...odds)
-		if result == 1 {
-			
-			decreaseOdds()
-			losses = 0
-			isShowingRewards = true
-		} else if losses >= odds {
-			
-			losses = 0
-			isShowingRewards = true
-		} else {
-			
-			losses += 1
-			isAlerting = true
-		}
-	}
-	
-	private func decreaseOdds() {
-		if odds < Constants.maxOdds {
-			odds += 1
-		}
-	}
-	
-	private func populateRewards() async {
-		
-		if rewards.isEmpty {
-			
-			Reward.allCases.forEach {
-				let newReward = SDReward(name: $0.description, systemImage: $0.image)
-				modelContext.insert(newReward)
-			}
-			
-			do {
-				try modelContext.save()
-			} catch {
-				print("error saving log - \(error.localizedDescription)")
-			}
-		}
-	}
+        
+        .task {
+            await populateRewards()
+            selectedUserManager.ensureSelectedUser(context: modelContext)
+        }
+    }
+    
+    private func populateRewards() async {
+        
+        if rewards.isEmpty {
+            
+            Reward.allCases.forEach {
+                let newReward = SDReward(name: $0.description, systemImage: $0.image)
+                modelContext.insert(newReward)
+            }
+            
+            do {
+                try modelContext.save()
+            } catch {
+                print("error saving log - \(error.localizedDescription)")
+            }
+        }
+    }
 }
 
+struct RollView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var rewards: [SDReward]
+    @Environment(\.themeColor) private var themeColor
+    
+    var selectedUser: SDUser?
+    
+    @State private var isShowingRewards = false
+    @State private var isAlerting = false
+    
+    var body: some View {
+        VStack {
+            Button("Smart Choice", action: { roll(for: selectedUser) })
+                .bold()
+                .fontDesign(.rounded)
+        }
+        .alert(
+            Alert.unlucky.title,
+            isPresented: $isAlerting,
+            presenting: Alert.unlucky
+        ) { state in
+            Button(state.cta) { }
+        } message: { state in
+            Text(state.subtitle)
+        }
+        .sheet(isPresented: $isShowingRewards) {
+            RewardListView(modelContext: self.modelContext)
+        }
+    }
+    
+    private func roll(for user: SDUser?) {
+        
+        let result = if let user {
+            Roll.perform(for: user)
+        } else {
+            Roll.perform()
+        }
+        
+        if result {
+            isShowingRewards = true
+        } else {
+            isAlerting = true
+        }
+    }
+}
+
+
 #Preview {
-	ContentView()
+    ContentView()
 }
--- obeyWatch Watch App/SwitchUserView.swift
+++ obeyWatch Watch App/SwitchUserView.swift
@@ -2,44 +2,70 @@ import SwiftData
 import SwiftUI
 
 struct SwitchUserView: View {
-        @Environment(\.modelContext) private var modelContext
-        @Query(sort: \SDUser.name) private var users: [SDUser]
-
-        @ObservedObject private var selectedUserManager = SelectedUserManager.shared
-        @ObservedObject private var userViewModel = UserViewModel.shared
-        @Environment(\.themeColor) private var themeColor
-
-        var body: some View {
-                if userViewModel.unlockActive {
-                        List(users) { user in
-                                Button(action: { selectedUserManager.selectedUser = user }) {
-                                        HStack {
-                                                Text(user.name)
-                                                if selectedUserManager.selectedUser == user {
-                                                        Spacer()
-                                                        Image(systemName: "checkmark")
-                                                                .foregroundStyle(themeColor)
-                                                }
-                                        }
-                                }
-                                .tint(themeColor)
-                        }
-                        .listStyle(.carousel)
-                } else {
-                        VStack {
-                                Spacer()
-                                Image(systemName: "lock.fill")
-                                        .font(.largeTitle)
-                                        .foregroundStyle(themeColor)
-                                Text("Subscribe to unlock multiple users")
-                                        .multilineTextAlignment(.center)
-                                        .padding()
-                                Spacer()
-                        }
-                }
+    
+    @ObservedObject private var userViewModel = UserViewModel.shared
+    
+    var body: some View {
+        NavigationStack {
+            if true { //userViewModel.unlockActive {
+                SelectUserListView()
+            } else {
+                UnlockView()
+            }
         }
+    }
+}
+
+struct SelectUserListView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \SDUser.name) private var users: [SDUser]
+    
+    @ObservedObject private var selectedUserManager = SelectedUserManager.shared
+    
+    @Environment(\.themeColor) private var themeColor
+    
+    var body: some View {
+        List(users) { user in
+            Button {
+                selectedUserManager.selectedUser = user
+                dismiss()
+            } label: {
+                HStack {
+                    Text(user.name)
+                    if selectedUserManager.selectedUser == user {
+                        Spacer()
+                        Image(systemName: "checkmark")
+                            .foregroundStyle(themeColor)
+                    }
+                }
+            }
+            .tint(themeColor)
+        }
+        .listStyle(.carousel)
+        .navigationTitle("Select child")
+    }
+}
+
+struct UnlockView: View {
+    
+    @Environment(\.themeColor) private var themeColor
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Image(systemName: "lock.fill")
+                .font(.largeTitle)
+                .foregroundStyle(themeColor)
+            Text("Subscribe to unlock multiple users")
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle("Unlock")
+    }
 }
 
 #Preview {
-        SwitchUserView()
+    SwitchUserView()
 }
