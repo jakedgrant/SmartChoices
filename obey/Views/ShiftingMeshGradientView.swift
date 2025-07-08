5e1466e43e6cde90420479c32df57030c9b72bea
Jake Grant <8376905+jakedgrant@users.noreply.github.com>
2025-07-07 19:18:00 -0500

    Add gradients and reorg settings view (#43)

--- obey/Views/ShiftingMeshGradientView.swift
+++ obey/Views/ShiftingMeshGradientView.swift
@@ -31,9 +31,46 @@ struct ShiftingMeshGradientView: View {
             .animation(.easeInOut, value: color)
             .overlay(.ultraThinMaterial)
         }
-        
     }
 }
+
+struct AttentionMeshGradient: View {
+    
+    let primary: Color
+    let secondary: Color
+    
+    init(_ primary: Color, _ secondary: Color) {
+        self.primary = primary
+        self.secondary = secondary
+    }
+    
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            let x = 0.25 + (sin(timeline.date.timeIntervalSince1970) + 1) / 4
+            let y = 0.25 + (cos(timeline.date.timeIntervalSince1970) + 1) / 4
+            
+            MeshGradient(width: 3, height: 3, points: [
+                [0, 0], [Float(y), 0], [1, 0],
+                [0, Float(x)], [Float(x), Float(y)], [1, Float(y)],
+                [0, 1], [Float(y), 1], [1, 1]
+            ], colors: [
+                primary, secondary, secondary,
+                primary, primary, primary,
+                secondary, primary, secondary
+            ])
+            .animation(.easeInOut, value: primary)
+//            .overlay(.ultraThinMaterial)
+        }
+    }
+}
+
 #Preview {
-    ShiftingMeshGradientView(color: .red)
+    Form {
+        Label("Subscribe now to unlock all features and new ways to reward good behavior!", systemImage: "star.circle")
+            .bold()
+            .foregroundStyle(.white)
+            .listRowBackground(
+                AttentionMeshGradient(.mint, .black.opacity(0.55))
+            )
+    }
 }
