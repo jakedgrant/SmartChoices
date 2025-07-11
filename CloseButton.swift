b1446bd84fe8f2889aea558c98f379c0e5ef96cb
jakedgrant <jakegrant@hey.com>
2025-07-10 20:09:25 -0500

    clean it

--- CloseButton.swift
+++
@@ -1,8 +0,0 @@
-Button {
-						dismiss()
-					} label: {
-						Image(systemName: "xmark")
-							.imageScale(.small)
-					}
-					.buttonStyle(SCCircleButtonStyle(padding: 12))
-					.padding(.trailing, -12)
