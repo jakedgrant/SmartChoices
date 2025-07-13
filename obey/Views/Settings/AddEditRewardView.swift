6a31c82e1c55d67f8c4e9837b63baa4cd0bd6183
jakedgrant <jakegrant@hey.com>
2025-07-13 18:18:13 -0500

    Update icon picker view

--- obey/Views/Settings/AddEditRewardView.swift
+++ obey/Views/Settings/AddEditRewardView.swift
@@ -40,7 +40,7 @@ struct AddEditRewardView: View {
                     IconView(icon: reward.systemImage)
                 }
                 .sheet(isPresented: $isShowingIconPicker) {
-                    IconPickerView(selectedImageName: $reward.systemImage)
+                    IconPickerView(selectedImageName: $reward.systemImage, tintColor: themeColor)
                         .presentationDetents([.medium])
                 }
             }
--- obey/Views/Settings/IconPickerView.swift
+++ obey/Views/Settings/IconPickerView.swift
@@ -9,57 +9,84 @@ import SwiftUI
 
 struct IconPickerView: View {
 	@Binding var selectedImageName: String
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var tintColor: Color
 	
 	let columns = [
 		GridItem(.adaptive(minimum: 60, maximum: 150))
 	]
 	
     var body: some View {
-		ScrollView {
-			
-			ForEach(Icon.default.sets) { set in
-			
-				LazyVGrid(
-					columns: columns,
-					spacing: 8,
-					pinnedViews: .sectionHeaders
-				) {
-					Section {
-						ForEach(set.iconNames, id: \.self) { name in
-							
-							Button {
-								selectedImageName = name
-							} label: {
-								IconView(icon: name)
-									.overlay(
-										RoundedRectangle(cornerRadius: 8)
-											.stroke(.accent, lineWidth: selectedImageName == name ? 4 : 0)
-									)
-							}
-							.padding(.bottom, 8)
-						}
-					} header: {
-						
-						VStack(spacing: 2) {
-							HStack {
-								Text(set.name)
-									.fontDesign(.rounded)
-									.foregroundStyle(.accent)
-									.bold()
-								Spacer()
-							}
-							.padding(.leading, 8)
-							Divider()
-						}
-						.background(.gray.opacity(0.2))
-						.background(.white)
-					}
-				}
-			}
-		}
+        NavigationView {
+            ScrollView {
+                
+                ForEach(Icon.default.sets) { set in
+                    
+                    LazyVGrid(
+                        columns: columns,
+                        spacing: 8,
+                    ) {
+                        
+                        Section {
+                            ForEach(set.iconNames, id: \.self) { name in
+                                
+                                Button {
+                                    
+                                    withAnimation {
+                                        selectedImageName = name
+                                    }
+                                } label: {
+                                    
+                                    Image(systemName: name)
+                                        .frame(width: 58, height: 58)
+                                        .symbolRenderingMode(.hierarchical)
+                                        .background {
+                                            RoundedRectangle(cornerRadius: 8)
+                                                .opacity(selectedImageName == name ? 0.2 : 0.0)
+                                        }
+                                }
+                                .tint(selectedImageName == name ? tintColor : .primary)
+                                .sensoryFeedback(.selection, trigger: selectedImageName)
+                            }
+                        } header: {
+                            
+                            VStack(spacing: 2) {
+                                HStack {
+                                    Text(set.name)
+                                        .fontDesign(.rounded)
+                                        .foregroundStyle(.gray)
+                                        .bold()
+                                    Spacer()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 12)
+            .navigationTitle("Icons")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    CloseButton { dismiss() }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-	IconPickerView(selectedImageName: .constant("figure.run"))
+struct IconPickerView_Previews: PreviewProvider {
+    struct Wrapper: View {
+        @State var selected = Icon.default.sets.first?.iconNames.first ?? "plane"
+        
+        var body: some View {
+            IconPickerView(selectedImageName: $selected, tintColor: .blue)
+        }
+    }
+    
+    static var previews: some View {
+        Wrapper()
+    }
 }
